bd8cb8dc27720396995de1ae437cba3000030b7f
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-08-04 18:01:31 +0300

    change dependecies in db schema + db models

--- backend/models/models.js
+++ backend/models/models.js
@@ -13,3 +13,36 @@ const Basket = db.define('basket', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 });
 
+const BasketDevice = db.define('basket_device', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+
+const Device = db.define('device', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+    price: { type: DataTypes.FLOAT, allowNull: false },
+    rating: { type: DataTypes.INTEGER, defaultValue: 0 },
+    img: {type: DataTypes.STRING, allowNull: false }
+});
+
+const Type = db.define('type', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const Brand = db.define('brand', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const Rating = db.define('rating', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    rate: { type: DataTypes.INTEGER, allowNull: false }, 
+});
+
+const DeviceInfo = db.define('device_info', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: { type: DataTypes.STRING, allowNull: false },
+});
+
