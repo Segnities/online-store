48f54d61d5cfce5cac64a97711baabbfb774f46e
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-08-05 18:14:10 +0300

    API errors handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,9 +3,16 @@ class UserController {
     }
 
     async auth(req, res) {
+        const { id } = req.body;
+        
+        if (!id) {
+            res.status(400).json({ message: 'Id is not found' });
+        }
+
+        res.json({ message: 'Auth success!' });
+
     }
     async login(req, res) {
-        res.json({ message: 'Login success!' });
 
     }
 }
