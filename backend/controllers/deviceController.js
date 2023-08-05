d463e8e92f9b5cd4aa36def1ef528ce8f125a115
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-08-05 18:06:30 +0300

    rest controllers

---
+++ backend/controllers/deviceController.js
@@ -0,0 +1,11 @@
+class DeviceRouter {
+    async getAll() {
+
+    }
+    async getOne() {}
+
+    async create() {}
+}
+
+
+module.exports = new DeviceRouter();
--- backend/routes/deviceRouter.js
+++ backend/routes/deviceRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+const DeviceRouter = require('../controllers/deviceController');
 
-router.post('/');
-router.get('/');
-router.get('/:id');    
+router.post('/', DeviceRouter.create);
+router.get('/', DeviceRouter.getAll);
+router.get('/:id', DeviceRouter.getOne);    
 
 module.exports = router;
