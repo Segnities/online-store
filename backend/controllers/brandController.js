3a7af6889d9fba57503bbf24f2fd2b9cc8f790c1
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-08-13 15:18:32 +0300

    delete + update brand

--- backend/controllers/brandController.js
+++ backend/controllers/brandController.js
@@ -1,3 +1,4 @@
+const ApiError = require('../error/ApiError');
 const { Brand } = require('../models/models');
 
 class BrandController {
@@ -10,6 +11,28 @@ class BrandController {
         const { name } = req.body;
         const brand = await Brand.create({ name });
         return res.json(brand);
+    }
+
+    async update(req, res, next) {
+        try {
+            const { name } = req.body;
+            const { id } = req.params;
+            const brand = await Brand.update({ name }, { where: { id } });
+            return res.json(brand);
+        } catch (error) {
+            next(ApiError.badRequest(error.message));
+        }
+    }
+
+    async delete(req, res, next) {
+        try {
+            const { id } = req.params;
+            const brand = await Brand.destroy({ where: { id } });
+            return res.json(brand);
+        }
+        catch (error) {
+            next(ApiError.badRequest(error.message));
+        }
     }
 }
 
--- backend/routes/brandRouter.js
+++ backend/routes/brandRouter.js
@@ -6,6 +6,7 @@ const checkRoleMiddleware = require('../middleware/checkRoleMiddleware');
 
 router.post('/', checkRoleMiddleware('ADMIN'), BrandController.create);
 router.get('/', BrandController.getAll);
-
+router.put('/:id', checkRoleMiddleware('ADMIN'), BrandController.update);
+router.delete('/:id', checkRoleMiddleware('ADMIN'), BrandController.delete);
 
 module.exports = router;
