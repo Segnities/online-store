1cfed477250d6c1556221b43842ef21ed937ec45
unknown <dimab787898@gmail.com>
2023-09-04 20:02:07 +0300

    save token to localStorage

--- frontend/src/http/userAPI.ts
+++ frontend/src/http/userAPI.ts
@@ -1,20 +1,23 @@
 import { $authHost, $defaultHost } from "./index";
 import jwt_decode from "jwt-decode";
+import type { UserData } from "@/store/UserStore";
 
-export const registration = async (email: string, password: string) => {
+export const registration = async (email: string, password: string):Promise<UserData> => {
    const { data } = await $authHost.post('api/user/registration', {
       email,
       password,
       role: 'ADMIN'
    });
+   localStorage.setItem('token', data.jwt);
    return jwt_decode(data.jwt);
 }
 
-export const login = async (email: string, password: string) => {
+export const login = async (email: string, password: string):Promise<UserData> => {
    const { data } = await $authHost.post('api/user/login', {
       email,
       password,
    });
+   localStorage.setItem('token', data.jwt);
    return jwt_decode(data.jwt);
 }
 
